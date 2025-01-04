bad449c879c0ba0ea2f062467b5335538df964ab
James Wu <jameswu@Jamess-MacBook-Air-2.local>
2025-01-04 18:05:03 +0800

    Complete course scraping script, script overview in readme

---
+++ api/README.md
@@ -0,0 +1,55 @@
+# API Directory README
+
+## Overview
+
+This directory contains executable scripts that are essential for the functionality of the WashU Degree Planner API. Below is a detailed explanation of each script and its purpose.
+
+## Scripts
+
+### `serve`
+
+- **Description**: This script starts the API server.
+- **Usage**:
+
+  ```
+  npm run serve
+  ```
+
+### `populate-schools`
+
+- **Description**: This script populates the database with the 5 schools based on the data.json file.
+- **Usage**:
+
+  ```
+  npm run populate-schools
+  ```
+
+### `scrape-departments`
+
+- **Description**: This script populates the database with all academic departments in the 5 schools from WebSTAC. It does not scrape the department identifier, e.g. CSE for Computer Science Department, which is done in the course scraping script.
+- **Usage**:
+
+  ```
+  npm run scrape-departments
+  ```
+
+### `scrape-courses`
+
+- **Description**: This script populates the database with all courses available during a given semester from WebSTAC. It also populates the database with the necessary attribute tags and creates the department identifier for each department. It does not scrape courses with variable credit hours.
+- **Usage**:
+
+  ```
+  npm run scrape-courses [semester] [school]
+  ```
+
+  -- **semester**: optional integer argument to specify the semester from which to scrape courses, 1-indexed. Value `1` is the most recently available semester (currently SP25), while `2` is the next most recent semester, and so on. Default value is `1`.
+  -- **school**: optional integer argument to specify which school to scrape courses from, of the 5 schools specified in the data.json file, 1-indexed. Value between 1 and 5, inclusive. Default is to scrape all 5 schools. Must specify **semester** argument to use this argument.
+
+## Notes
+
+- Ensure you have the necessary permissions to execute these scripts.
+- Modify the scripts as needed to fit your environment and requirements.
+
+## Contact
+
+For any issues or questions, please contact the project maintainer.
--- api/src/scripts/scrapeCourses.js
+++ api/src/scripts/scrapeCourses.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer"); //must have chromium
+const lodash = require("lodash");
 const schools = require("./data.json").schools;
 const prisma = require("./../models/index");
 
@@ -20,6 +21,21 @@ function splitNameAndCode(input) {
   }
 
   throw new Error("Invalid name and code string");
+}
+
+function splitCourseCodeString(code) {
+  const regex = /^(\S+)(?:\s+(.*\s+)?(\S+))?$/;
+  const match = code.match(regex);
+
+  if (!match) {
+    throw new Error("Invalid course code string format.");
+  }
+
+  return [match[1], match[2].trim(), match[3]];
+}
+
+function extractCredits(str) {
+  return parseFloat(str.split(" ", 2)[0]);
 }
 
 async function main(args) {
@@ -65,8 +81,20 @@ async function main(args) {
     text = await innerHTML(page, title);
   }
 
-  for (let school of schools) {
-    console.log(school.name);
+  let schoolsIterable = schools;
+
+  if (args.length > 3) {
+    if (parseInt(args[3]) < 1 || parseInt(args[3]) > 5) {
+      throw new Error(
+        "Expected school argument as integer between 1 and 5, inclusive. Instead recieved " +
+          args[3]
+      );
+    }
+    schoolsIterable = [schools[parseInt(args[3]) - 1]];
+  }
+
+  for (let school of schoolsIterable) {
+    console.log("Started scraping school: " + school.name);
 
     const [navigationResult] = await Promise.all([
       page
@@ -92,7 +120,7 @@ async function main(args) {
     });
 
     const min =
-      school.name === "Arts & Sciences" ? 0 : school.name === "Art" ? 2 : 1;
+      school.name === "Arts & Sciences" ? 0 : school.name === "Art" ? 2 : 1; //first valid department within school
 
     for (let i = min; i < numDepts; ++i) {
       const deptButton = await page.$(`#Body_dlDepartments_lnkDept_${i}`);
@@ -102,37 +130,49 @@ async function main(args) {
       const deptNameCode = await innerHTML(page, deptButton);
       const deptNameCodeFormatted = deptNameCode.replace(/&amp;/g, "&");
 
-      const { code } = splitNameAndCode(deptNameCodeFormatted);
+      const { name, code } = splitNameAndCode(deptNameCodeFormatted);
+      const capitalizedName = lodash.startCase(lodash.lowerCase(name));
+
+      console.log("Started scraping department: " + capitalizedName);
 
       await deptButton.click();
-      await page.waitForSelector("#tabDeptBar0");
 
-      let deptBarText = await page.$eval("#tabDeptBar0", (table) => {
-        const link = table.querySelector("td:nth-child(2) > a.RedLink");
-        return link ? link.textContent.trim() : null;
-      });
+      try {
+        await page.waitForSelector("#tabDeptBar0");
 
-      let counter = 0;
-      while (!deptBarText.includes(code)) {
-        await sleep();
-        deptBarText = await page.$eval("#tabDeptBar0", (table) => {
+        let deptBarText = await page.$eval("#tabDeptBar0", (table) => {
           const link = table.querySelector("td:nth-child(2) > a.RedLink");
           return link ? link.textContent.trim() : null;
         });
-        ++counter;
-        if (counter > 30) {
-          throw new Error(
-            "Waiting for new department courses timed out after 30000 ms."
-          );
+
+        let counter = 0;
+        while (!deptBarText.includes(code)) {
+          await sleep();
+          deptBarText = await page.$eval("#tabDeptBar0", (table) => {
+            const link = table.querySelector("td:nth-child(2) > a.RedLink");
+            return link ? link.textContent.trim() : null;
+          });
+          ++counter;
+          if (counter > 30) {
+            throw new Error(
+              "Waiting for new department courses timed out after 30000 ms."
+            );
+          }
         }
+      } catch (error) {
+        //no courses listed or timed out
+        console.error(error);
+
+        continue;
       }
 
       const coursesDiv = await page.$("#Body_oCourseList_tabSelect");
       const courses = await coursesDiv.$$(":scope > div");
 
+      let isIdentifierSet = false;
+      let departmentId;
+
       for (let course of courses) {
-        // const innerText = await course.evaluate((el) => el.innerText);
-        // console.log("InnerText of course:", innerText);
         const [code, name, units] = await course.$eval(
           "td:nth-child(2)",
           (td) => {
@@ -144,20 +184,95 @@ async function main(args) {
           }
         );
 
-        const [description, attributes] = await course.$eval(
-          ".DivDetail table",
-          (div) => {
-            return [
-              div.querySelector("table:nth-of-type(1)").textContent.trim(),
-              div.querySelector("table:nth-of-type(2)").textContent.trim(),
-            ];
+        const [deptCode, deptIdentifier, courseCode] =
+          splitCourseCodeString(code);
+
+        const credits = extractCredits(units);
+
+        if (!isIdentifierSet) {
+          try {
+            const result = await prisma.department.update({
+              where: {
+                code: deptCode,
+              },
+              data: {
+                identifier: deptIdentifier,
+              },
+            });
+
+            departmentId = result.id;
+          } catch (error) {
+            console.error(error);
           }
-        );
-        console.log(code);
-        console.log(name);
-        console.log(units);
-        console.log(description);
-        console.log(attributes);
+          isIdentifierSet = true;
+        }
+
+        const description = await course.$eval(".DivDetail table", (div) => {
+          return div.querySelector("table:nth-of-type(1)").textContent.trim();
+        });
+
+        const descriptionTrimmed = description.split("Description:", 2)[1];
+
+        const attributesTable = await course.$("table:nth-of-type(2)");
+        const attributesData = await attributesTable.$("td:nth-of-type(2)");
+        const attributeNodes = await attributesData.$$("a");
+
+        let attributeKey;
+        const attributes = [];
+
+        for (let attribute of attributeNodes) {
+          const isAttrKey = await attribute.evaluate((element, className) => {
+            return element.classList.contains(className);
+          }, "CrsAttr");
+
+          const text = await attribute.evaluate((element) =>
+            element.innerText.trim()
+          );
+
+          if (isAttrKey) {
+            attributeKey = text;
+          } else {
+            attributes.push(attributeKey + ": " + text);
+          }
+        }
+
+        try {
+          await Promise.all(
+            attributes.map(async (attribute) => {
+              const result = await prisma.tag.upsert({
+                where: { name: attribute },
+                update: {},
+                create: { name: attribute },
+              });
+            })
+          );
+
+          const result = await prisma.course.upsert({
+            where: {
+              departmentId_courseCode: {
+                departmentId: parseInt(departmentId),
+                courseCode: courseCode,
+              },
+            },
+            update: {},
+            create: {
+              title: name,
+              courseCode: courseCode,
+              credits: credits,
+              department: {
+                connect: {
+                  id: departmentId,
+                },
+              },
+              description: descriptionTrimmed,
+              tags: {
+                connect: attributes.map((name) => ({ name })),
+              },
+            },
+          });
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
   }
